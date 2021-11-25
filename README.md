d0b83cc72b9216a3eb6dfab63e45bfe796f5b765
Alexandre Dulaunoy <a@foo.be>
2021-11-25 07:31:46 +0100

    new: [hashlookup-json-importer] Generic NDJSON importer for hashlookup server

---
+++ README.md
@@ -0,0 +1,26 @@
+# Generic NDJSON importer for hashlookup server
+
+# Usage
+
+~~~~
+python3 hashlookup-json-importer.py --help
+usage: hashlookup-json-importer.py [-h] [-v] [-s SOURCE] [-p PARENT] [--parent-meta PARENT_META [PARENT_META ...]] [-u] [-e]
+
+Generic NDJSON importer for hashlookup server
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         Verbose output
+  -s SOURCE, --source SOURCE
+                        Source name to be used as meta
+  -p PARENT, --parent PARENT
+                        Parent SHA-1 of the import
+  --parent-meta PARENT_META [PARENT_META ...]
+                        Add metadata to parent, format is key,value
+  -u, --update          Update hash if it already exists. default is not to update existing hashlookup record but to delete existing records and update.
+  -e, --skip-exists     Skip import of existing hashlookup record
+~~~~
+
+The input must be a NDJSON file with the standard hashlookup JSON format.
+
+`cat yourstream.json | python3 hashlookup-json-importer.py -v``
---
+++ bin/hashlookup-json-importer.py
@@ -0,0 +1,72 @@
+import hashlookup.hashlookup as hashlookup
+import argparse
+import sys
+from glob import glob
+import os
+import json
+
+BUF_SIZE = 65536
+
+parser = argparse.ArgumentParser(description="Generic NDJSON importer for hashlookup server")
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output", default=False)
+parser.add_argument(
+    "-s", "--source", help="Source name to be used as meta", default="hashlookup-json-import"
+)
+parser.add_argument("-p", "--parent", help="Parent SHA-1 of the import", default=None)
+parser.add_argument(
+    "--parent-meta",
+    help="Add metadata to parent, format is key,value",
+    default=None,
+    nargs='+',
+)
+parser.add_argument(
+    "-u",
+    "--update",
+    help="Update hash if it already exists. default is not to update existing hashlookup record but to delete existing records and update.",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "-e",
+    "--skip-exists",
+    action="store_true",
+    default=False,
+    help="Skip import of existing hashlookup record",
+)
+
+args = parser.parse_args()
+
+
+if not args.update:
+    h = hashlookup.HashLookupInsert(
+        update=False, source=args.source, skipexists=args.skip_exists, publish=True
+    )
+else:
+    h = hashlookup.HashLookupInsert(
+        update=True, source=args.source, skipexists=args.skip_exists, publish=True
+    )
+
+if args.verbose:
+    v = h.get_version()
+    print(f"hashlookup-lib version: {v}")
+
+for line in sys.stdin:
+    hashes = ['md5', 'sha-1', 'sha-256', 'sha-512', 'tlsh', 'ssdeep']
+    record = json.loads(line)
+    for key in record.keys():
+        if key in hashes or key.lower() in hashes:
+            k = key.upper()
+            if record[key] == "":
+                continue
+            h.add_hash(value=record[key], hashtype=k)
+            continue
+        h.add_meta(key=key, value=record[key])
+    if args.parent is not None:
+        h.add_parent(value=args.parent)
+    if args.parent is not None and args.parent_meta is not None:
+        for pmeta in args.parent_meta:
+            k, v = pmeta.split(",")
+            h.add_parent_meta(value=args.parent, meta_key=k, meta_value=v)
+    r = h.insert()
+    if args.verbose:
+        print(f"Imported -> {r}")
